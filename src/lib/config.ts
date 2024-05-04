543283942f4ac5fe465e5d560f79422cd08d5b1e
joetroll <161910543+trolljoe@users.noreply.github.com>
2024-05-04 15:02:29 -0300

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@ import { dev } from "$app/environment";
 
 export const title = "joetroll's sick ass blog";
 export const description = 'blog made with sveltekit'
-export const url = dev ? 'http://localhost:5173/' : 'blogjoetroll.pages.dev'
+export const url = dev ? 'http://localhost:5173/' : 'blogtroll.pages.dev'
